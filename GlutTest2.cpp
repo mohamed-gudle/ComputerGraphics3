fa81df532789ae59482246790db1d42c9802f499
mikemwangi <michael.thuita@strathmore.edu>
2021-11-13 22:41:55 +0300

    River and fish added. Fixed window scaling issue

--- GlutTest2.cpp
+++ GlutTest2.cpp
@@ -8,26 +8,86 @@
 #include <iostream>
 // Clears the current window and draws a triangle.
 using namespace std;
-void display() {
-	// Set every pixel in the frame buffer to the current clear color.
 
-glClearColor(255, 255, 255, 255);
-glClear(GL_COLOR_BUFFER_BIT);
-glColor3f(1, 0, 0);
-glOrtho(-3.0, 3.0, -3.0, 3.0, -1.0, 1.0);
+void init()
+{
+	glClearColor(255, 255, 255, 255);
 
-// Drawing is done by specifying a sequence of vertices. The way these
-	// vertices are connected (or not connected) depends on the argument to
-	// glBegin. GL_POLYGON constructs a filled polygon.
-GLint circle_points = 420;
-double PI = 3.1415926535898;
-glBegin(GL_LINE_LOOP);
-for (int i = 0; i < circle_points/2; i++) {
-	double angle= 2 * PI * i / circle_points;
-	glVertex2f(cos(angle), sin(angle));
+
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+	 glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(45,640.0/480.0,1.0,500.0);
+    glMatrixMode(GL_MODELVIEW);
+    glEnable(GL_DEPTH_TEST);
+
+
 
 }
 
+
+// A fish bitmap, size is 27x11, but all rows must have a multiple of 8 bits,
+// so we define it like it is 32x11.
+GLubyte fish[] = {
+  0x00, 0x60, 0x01, 0x00,
+  0x00, 0x90, 0x01, 0x00,
+  0x03, 0xf8, 0x02, 0x80,
+  0x1c, 0x37, 0xe4, 0x40,
+  0x20, 0x40, 0x90, 0x40,
+  0xc0, 0x40, 0x78, 0x80,
+  0x41, 0x37, 0x84, 0x80,
+  0x1c, 0x1a, 0x04, 0x80,
+  0x03, 0xe2, 0x02, 0x40,
+  0x00, 0x11, 0x01, 0x40,
+  0x00, 0x0f, 0x00, 0xe0,
+};
+
+
+// Return a random float in the range 0.0 to 1.0.
+GLfloat randomFloat() {
+  return (GLfloat)rand() / RAND_MAX;
+}
+
+// On reshape, uses an orthographic projection with world coordinates ranging
+// from 0 to 1 in the x and y directions, and -1 to 1 in z.
+void reshape(int width, int height) {
+  glViewport(0, 0, width, height);
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  gluOrtho2D(1, 1, 1, 1);
+}
+
+void display() {
+// Set every pixel in the frame buffer to the current clear color.
+glClear(GL_COLOR_BUFFER_BIT);
+
+
+// glColor3f(1, 0, 0);
+glOrtho(-4.5, 4.5, -4.5, 4.5, -1.0, 1.0);
+
+
+
+// Plots 20 fish bitmaps in random colors at random positions
+ for (int i = 0; i < 20; i++) {
+    glColor3f(randomFloat(), randomFloat(), randomFloat());
+    // glRasterPos3f(randomFloat(), randomFloat(), 0.0);
+	glRasterPos3f(randomFloat(), randomFloat()-3.9, 0.0);
+    glBitmap(27, 11, 0, 0, 0, 0, fish);
+  }
+
+// Drawing is done by specifying a sequence of vertices. The way these
+// vertices are connected (or not connected) depends on the argument to
+// glBegin. GL_POLYGON constructs a filled polygon.
+GLint circle_points = 420;
+double PI = 3.1415926535898;
+	glBegin(GL_LINE_LOOP);
+	for (int i = 0; i < circle_points/2; i++) {
+		double angle= 2 * PI * i / circle_points;
+		glVertex2f(cos(angle), sin(angle));
+
+	}
 	glEnd();
 
     // Roof frame
@@ -37,6 +97,46 @@ for (int i = 0; i < circle_points/2; i++) {
 	glVertex2f(1.5, -0.5);
 	glVertex2f(-1.5, -0.5);
 	glEnd();
+
+	// //River
+	// glBegin(GL_QUADS);
+	// glColor4f(0.0, 0.0, 1.0, 1.0);
+    // glVertex2f(4.0, -4.0);
+	// glVertex2f(-4, -4.0);
+	// glVertex2f(-4, -3.0);
+	// glVertex2f(4, -3.0);
+    // glEnd();
+
+
+	// //River 2
+	// glBegin(GL_QUADS);
+	// glColor4f(1.0, 0.0, 0.0, 0.5);
+    // glVertex2f(2.0, -4.0);
+	// glVertex2f(-3.5, -4.0);
+	// glVertex2f(-3, -3.0);
+	// glVertex2f(2, -3.0);
+    // glEnd();
+
+    glBegin(GL_QUADS);
+    // draw transparent objects from far to near!
+    // first draw transparent objects then nontransparent objects
+
+
+        glColor4f(0, 0, 1, .3);
+        glVertex3f(4.5, -4, 1);
+        glVertex3f(-4.5, -4, 1);
+        glVertex3f(-4.5, -2.6, 1);
+        glVertex3f(4.5, -2.6, 1);
+    glEnd();
+
+// 		glBegin(GL_POLYGON);
+//     glColor3f(1, 0, 0);
+// 	glVertex3f(-0.6, -0.75, 0.5);
+//     glColor3f(0, 1, 0);
+// 	glVertex3f(0.6, -0.75, 0);
+//     glColor3f(0, 0, 1);
+// 	glVertex3f(0, 0.75, 0);
+//   glEnd();
 
     // House Frame
 	glBegin(GL_LINE_LOOP);
@@ -53,7 +153,6 @@ for (int i = 0; i < circle_points/2; i++) {
 	glVertex2f(.3, -1.25);
 	glVertex2f(.3, -2.5);
 	glEnd();
-
 
     //Door Knob
     float x1,y1,x2,y2;
@@ -72,7 +171,6 @@ for (int i = 0; i < circle_points/2; i++) {
         glVertex2f(x2,y2);
     }
     glEnd();
-
 
     // Window left frame
     glBegin(GL_LINE_LOOP);
@@ -114,27 +212,47 @@ for (int i = 0; i < circle_points/2; i++) {
 	glVertex2f(.5, -1.5);
 	glEnd();
 
-
-
 	// Flush drawing command buffer to make drawing happen as soon as possible.
 	glFlush();
 }
+
+
 // Initializes GLUT, the display mode, and main window; registers callbacks;
 // enters the main event loop.
 int main(int argc, char** argv) {
+
+
+
+
+
 	// Use a single buffered window in RGB mode (as opposed to a double-buffered
 	// window or color-index mode).
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+
 	// Position window at (80,80)-(480,380) and give it a title.
 	glutInitWindowPosition(80, 80);
-	glutInitWindowSize(400, 400);
+	glutInitWindowSize(600, 600);
 	glutCreateWindow("A Simple Triangle");
 	// Tell GLUT that whenever the main window needs to be repainted that it
 	// should call the function display().
+
+
+
+
+
+
 	glutDisplayFunc(display);
 	// Tell GLUT to start reading and processing events. This function
 	// never returns; the program only exits when the user closes the main
 	// window or kills the process.
+
+	init();
+
+
+	glutReshapeFunc(reshape);
+
+
+
 	glutMainLoop();
 }
