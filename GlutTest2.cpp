9c840eb5afe2469f0c588c5cd10fd867ca945ed5
gudle <gudlemohamed@gmail.com>
2021-11-22 11:21:59 +0300

    resort merge conflict

--- GlutTest2.cpp
+++ GlutTest2.cpp
@@ -1,8 +1,9 @@
-#ifdef __APPLE_CC__
-#include <GLUT/glut.h>
-#else
-#include <GL/glut.h>
+#ifdef WIN32
+#include <windows.h>
 #endif
+#include <GL/glut.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
 #include <math.h>
 #include<cstdlib>
 #include <iostream>
@@ -12,16 +13,13 @@ using namespace std;
 void init()
 {
 	glClearColor(255, 255, 255, 255);
-
-
-    glEnable(GL_BLEND);
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-
-	 glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluPerspective(45,640.0/480.0,1.0,500.0);
-    glMatrixMode(GL_MODELVIEW);
-    glEnable(GL_DEPTH_TEST);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	// glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	//gluPerspective(45,640.0/480.0,1.0,500.0);
+	glMatrixMode(GL_MODELVIEW);
+	//glEnable(GL_DEPTH_TEST);
 
 
 
@@ -47,52 +45,54 @@ GLubyte fish[] = {
 
 // Return a random float in the range 0.0 to 1.0.
 GLfloat randomFloat() {
-  return (GLfloat)rand() / RAND_MAX;
+	return (GLfloat)rand() / RAND_MAX;
 }
 
 // On reshape, uses an orthographic projection with world coordinates ranging
 // from 0 to 1 in the x and y directions, and -1 to 1 in z.
 void reshape(int width, int height) {
-  glViewport(0, 0, width, height);
-  glMatrixMode(GL_PROJECTION);
-  glLoadIdentity();
-  gluOrtho2D(1, 1, 1, 1);
+	glViewport(0, 0, width, height);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(1, 1, 1, 1);
 }
 
 void display() {
-// Set every pixel in the frame buffer to the current clear color.
-glClear(GL_COLOR_BUFFER_BIT);
-glColor3f(1, 0, 0);
-glOrtho(-3.0, 3.0, -3.0, 3.0, -1.0, 1.0);
+	// Set every pixel in the frame buffer to the current clear color.
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glColor3f(10, 0, 0);
+	glOrtho(-3.0, 3.0, -3.0, 3.0, -1.0, 1.0);
 
 
-// glColor3f(1, 0, 0);
-glOrtho(-4.5, 4.5, -4.5, 4.5, -1.0, 1.0);
+	// glColor3f(1, 0, 0);
 
 
 
-// Plots 20 fish bitmaps in random colors at random positions
- for (int i = 0; i < 20; i++) {
-    glColor3f(randomFloat(), randomFloat(), randomFloat());
-    // glRasterPos3f(randomFloat(), randomFloat(), 0.0);
-	glRasterPos3f(randomFloat(), randomFloat()-3.9, 0.0);
-    glBitmap(27, 11, 0, 0, 0, 0, fish);
-  }
 
-// Drawing is done by specifying a sequence of vertices. The way these
-// vertices are connected (or not connected) depends on the argument to
-// glBegin. GL_POLYGON constructs a filled polygon.
-GLint circle_points = 420;
-double PI = 3.1415926535898;
+	// Plots 20 fish bitmaps in random colors at random positions
+	for (int i = 0; i < 20; i++) {
+		glColor3f(randomFloat(), randomFloat(), randomFloat());
+		// glRasterPos3f(randomFloat(), randomFloat(), 0.0);
+		glRasterPos3f(randomFloat(), randomFloat() - 3.9, 0.0);
+		glBitmap(27, 11, 0, 0, 0, 0, fish);
+	}
+
+
+	// Drawing is done by specifying a sequence of vertices. The way these
+	// vertices are connected (or not connected) depends on the argument to
+	// glBegin. GL_POLYGON constructs a filled polygon.
+	GLint circle_points = 420;
+	double PI = 3.1415926535898;
 	glBegin(GL_LINE_LOOP);
-	for (int i = 0; i < circle_points/2; i++) {
-		double angle= 2 * PI * i / circle_points;
+	for (int i = 0; i < circle_points / 2; i++) {
+		double angle = 2 * PI * i / circle_points;
 		glVertex2f(cos(angle), sin(angle));
 
 	}
 	glEnd();
 
-    // Roof frame
+	// Roof frame
 	glBegin(GL_LINE_LOOP);
 	glVertex2f(-1.5, 0);
 	glVertex2f(1.5, 0);
@@ -103,52 +103,52 @@ double PI = 3.1415926535898;
 	// //River
 	// glBegin(GL_QUADS);
 	// glColor4f(0.0, 0.0, 1.0, 1.0);
-    // glVertex2f(4.0, -4.0);
+	// glVertex2f(4.0, -4.0);
 	// glVertex2f(-4, -4.0);
 	// glVertex2f(-4, -3.0);
 	// glVertex2f(4, -3.0);
-    // glEnd();
+	// glEnd();
 
 
 	// //River 2
 	// glBegin(GL_QUADS);
 	// glColor4f(1.0, 0.0, 0.0, 0.5);
-    // glVertex2f(2.0, -4.0);
+	// glVertex2f(2.0, -4.0);
 	// glVertex2f(-3.5, -4.0);
 	// glVertex2f(-3, -3.0);
 	// glVertex2f(2, -3.0);
-    // glEnd();
+	// glEnd();
 
-    glBegin(GL_QUADS);
-    // draw transparent objects from far to near!
-    // first draw transparent objects then nontransparent objects
+	glBegin(GL_QUADS);
+	// draw transparent objects from far to near!
+	// first draw transparent objects then nontransparent objects
 
 
-        glColor4f(0, 0, 1, .3);
-        glVertex3f(4.5, -4, 1);
-        glVertex3f(-4.5, -4, 1);
-        glVertex3f(-4.5, -2.6, 1);
-        glVertex3f(4.5, -2.6, 1);
-    glEnd();
+	glColor4f(0, 0, 1, .3);
+	glVertex3f(4.5, -4, 1);
+	glVertex3f(-4.5, -4, 1);
+	glVertex3f(-4.5, -2.6, 1);
+	glVertex3f(4.5, -2.6, 1);
+	glEnd();
 
-// 		glBegin(GL_POLYGON);
-//     glColor3f(1, 0, 0);
-// 	glVertex3f(-0.6, -0.75, 0.5);
-//     glColor3f(0, 1, 0);
-// 	glVertex3f(0.6, -0.75, 0);
-//     glColor3f(0, 0, 1);
-// 	glVertex3f(0, 0.75, 0);
-//   glEnd();
+	// 		glBegin(GL_POLYGON);
+	//     glColor3f(1, 0, 0);
+	// 	glVertex3f(-0.6, -0.75, 0.5);
+	//     glColor3f(0, 1, 0);
+	// 	glVertex3f(0.6, -0.75, 0);
+	//     glColor3f(0, 0, 1);
+	// 	glVertex3f(0, 0.75, 0);
+	//   glEnd();
 
-    // House Frame
+		// House Frame
 	glBegin(GL_LINE_LOOP);
 	glVertex2f(1.25, -0.5);
 	glVertex2f(-1.25, -0.5);
 	glVertex2f(-1.25, -2.5);
 	glVertex2f(1.25, -2.5);
-    glEnd();
+	glEnd();
 
-    // Door Frame
+	// Door Frame
 	glBegin(GL_LINE_LOOP);
 	glVertex2f(-.3, -2.5);
 	glVertex2f(-.3, -1.25);
@@ -156,68 +156,102 @@ double PI = 3.1415926535898;
 	glVertex2f(.3, -2.5);
 	glEnd();
 
-    //Door Knob
-    float x1,y1,x2,y2;
-    float angle;
-    double radius=.05;
+	//Door Knob
+	float x1, y1, x2, y2;
+	float angle;
+	double radius = .05;
 
-    x1 = 0.2,y1=-1.9;
+	x1 = 0.2, y1 = -1.9;
 
-    glBegin(GL_TRIANGLE_FAN);
-    glVertex2f(x1,y1);
+	glBegin(GL_TRIANGLE_FAN);
+	glVertex2f(x1, y1);
 
-    for (angle=1.0f;angle<361.0f;angle+=0.2)
-    {
-        x2 = x1+sin(angle)*radius;
-        y2 = y1+cos(angle)*radius;
-        glVertex2f(x2,y2);
-    }
-    glEnd();
+	for (angle = 1.0f; angle < 361.0f; angle += 0.2)
+	{
+		x2 = x1 + sin(angle) * radius;
+		y2 = y1 + cos(angle) * radius;
+		glVertex2f(x2, y2);
+	}
+	glEnd();
 
-    // Window left frame
-    glBegin(GL_LINE_LOOP);
+	// Window left frame
+	glBegin(GL_LINE_LOOP);
 	glVertex2f(-1.12, -1.25);
 	glVertex2f(-.5, -1.25);
 	glVertex2f(-.5, -1.75);
 	glVertex2f(-1.12, -1.75);
 	glEnd();
 
-    // Window left inner frame (Vertical)
-    glBegin(GL_LINE_LOOP);
+	// Window left inner frame (Vertical)
+	glBegin(GL_LINE_LOOP);
 	glVertex2f(-.8, -1.25);
 	glVertex2f(-.8, -1.75);
 	glEnd();
 
-    // Window left inner frame (Horizontal)
-    glBegin(GL_LINE_LOOP);
+	// Window left inner frame (Horizontal)
+	glBegin(GL_LINE_LOOP);
 	glVertex2f(-1.12, -1.5);
 	glVertex2f(-.5, -1.5);
 	glEnd();
 
-    // Window Right frame
-    glBegin(GL_LINE_LOOP);
+	// Window Right frame
+	glBegin(GL_LINE_LOOP);
 	glVertex2f(1.12, -1.25);
 	glVertex2f(.5, -1.25);
 	glVertex2f(.5, -1.75);
 	glVertex2f(1.12, -1.75);
 	glEnd();
 
-    // Window right inner frame (Vertical)
-    glBegin(GL_LINE_LOOP);
+	// Window right inner frame (Vertical)
+	glBegin(GL_LINE_LOOP);
 	glVertex2f(.8, -1.25);
 	glVertex2f(.8, -1.75);
 	glEnd();
 
-    // Window right inner frame (Horizontal)
-    glBegin(GL_LINE_LOOP);
+	// Window right inner frame (Horizontal)
+	glBegin(GL_LINE_LOOP);
 	glVertex2f(1.12, -1.5);
 	glVertex2f(.5, -1.5);
 	glEnd();
 
 
+	//MOON
+	//Circle 1 -> Black
+	//glTranslatef is used to position the circle
+	glTranslatef(2.0, 2.0, 0);
+	//glScalef - size of the circle
+	glScalef(0.3, 0.3, 0);
+
+
+	glBegin(GL_TRIANGLE_FAN);
+	glColor3ub(249, 215, 28);
+	for (int i = 0; i <= 500; i++) {
+		double angle = 2 * PI * i / 500;
+		double x = cos(angle);
+		double y = sin(angle);
+		glVertex2d(x, y);
+	}
+	glEnd();
+
+	//Circle 2 -> white
+	glTranslatef(0.5, 0.5, 0);
+	glBegin(GL_TRIANGLE_FAN);
+	glColor3ub(255, 255, 255);
+
+	for (int i = 0; i <= 500; i++) {
+		double angle = 2 * PI * i / 500;
+		double x = cos(angle);
+		double y = sin(angle);
+		glVertex2d(x, y);
+	}
+
+	glEnd();
+
+
+
 
 	// Flush drawing command buffer to make drawing happen as soon as possible.
-	glFlush();
+	glutSwapBuffers();
 }
 
 
@@ -232,12 +266,15 @@ int main(int argc, char** argv) {
 	// Use a single buffered window in RGB mode (as opposed to a double-buffered
 	// window or color-index mode).
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
 
 	// Position window at (80,80)-(480,380) and give it a title.
-	glutInitWindowPosition(80, 80);
-	glutInitWindowSize(600, 600);
-	glutCreateWindow("A Simple Triangle");
+	glutInitWindowPosition(100, 80);
+
+	glutCreateWindow("A HOUSE ");
+
+	glutInitWindowSize(1000, 1000);
+
 	// Tell GLUT that whenever the main window needs to be repainted that it
 	// should call the function display().
 
